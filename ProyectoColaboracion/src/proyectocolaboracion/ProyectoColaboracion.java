892ec23de0042fb824ed2d3422402aca865c53c2
DannyST89 <dsdevelopercr@outlook.com>
2022-04-22 17:04:26 -0600

    Se inicia proyecto de colaboración

---
+++ ProyectoColaboracion/src/proyectocolaboracion/ProyectoColaboracion.java
@@ -0,0 +1,12 @@
+
+package proyectocolaboracion;
+
+
+public class ProyectoColaboracion {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
